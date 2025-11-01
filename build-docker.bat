90d24eeced975e1b8b77a9c860c1e82a6ae8428f
Alireza Asadi <alireza.asadi@fedex.com>
2025-11-01 16:40:28 +0100

    created contianer app builder

--- build-docker.bat
+++ build-docker.bat
@@ -8,8 +8,21 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
+
+echo =========================================================
+echo To refresh the app version in the image, pyproject.toml must be updated first.
+echo This can be done automatically when git-commit.bat is run.
+echo So, make sure to run git-commit.bat before running this script to build the image.
+
 echo ==========================================================
 echo Building Docker image...
+
+set SCM_DO_BUILD_DURING_DEPLOYMENT=false
+set WEBSITES_DISABLE_ORYX=true
+
+call az webapp config appsettings set \
+  --name lion --resource-group rg-lion-app \
+  --settings SCM_DO_BUILD_DURING_DEPLOYMENT=false WEBSITES_DISABLE_ORYX=true
 
 set /p tag=Enter the Docker image tag (default is 'latest'):
 if "%tag%"=="" set tag=latest
